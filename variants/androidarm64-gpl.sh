5492099842a996ceda03567ce64fe93710cd1b70
Qr <17964609+dntsdnts@users.noreply.github.com>
2026-08-20 05:36:54 +0800

    ci: add Android arm64 ELF builds

---
+++ variants/androidarm64-gpl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source "$(dirname "$BASH_SOURCE")"/linux-install-static.sh
+source "$(dirname "$BASH_SOURCE")"/defaults-gpl.sh
+
+FFBUILD_RUST_TARGET="aarch64-linux-android"
---
+++ variants/androidarm64-lgpl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source "$(dirname "$BASH_SOURCE")"/linux-install-static.sh
+source "$(dirname "$BASH_SOURCE")"/defaults-lgpl.sh
+
+FFBUILD_RUST_TARGET="aarch64-linux-android"
